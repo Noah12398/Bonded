cc3ded96c61924d207a7d17d03704041510312ff
Noah12398 <149400947+Noah12398@users.noreply.github.com>
2025-05-28 12:32:27 +0530

    Login Page

---
+++ app/src/main/java/com/example/bonded/Login.kt
@@ -0,0 +1,39 @@
+package com.example.bonded
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.bonded.ui.theme.BondedTheme
+
+class Login : AppCompatActivity() {
+    private lateinit var editmail:EditText
+    private lateinit var editpassword:EditText
+    private lateinit var login:Button
+    private lateinit var signup:Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        editmail=findViewById(R.id.Email)
+        editpassword=findViewById(R.id.password)
+        login=findViewById(R.id.button)
+        signup=findViewById(R.id.signup)
+
+        signup.setOnClickListener{
+            val intent= Intent(this,Signup::class.java)
+            startActivity(intent)
+        }
+    }
+}
